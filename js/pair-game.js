266c269601269c713fdd9aa3cfbc690264ba643b
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-12-08 17:12:46 +0100

    card clicks and rotates working

--- js/pair-game.js
+++ js/pair-game.js
@@ -1,14 +1,56 @@
-'use strict';
+{
+    'use strict';
+    const iconArray = ['save', 'caret-right', 'envelope', 'caret-left', 'save', 'caret-right', 'envelope', 'caret-left', 'star', 'star'];
+    const getAnIconFromArray = () => iconArray.pop();
+    const cards = document.querySelectorAll('.card');
+    let pairCheckTempArray = [];
+
+    const addCardItem = (element) => element.innerHTML = `<i class="fa fa-${getAnIconFromArray()}"></i>`;
+    const addEventToCard = (element) =>
+        element.addEventListener('click', clickEvent, true);
+
+    const checkIsPair = () => pairCheckTempArray.reduce((prev, next) => prev.innerHTML === next.innerHTML);
 
 
-const cards = document.querySelectorAll('.card');
+    const clickEvent = (inputElement) => {
+        const element = inputElement.currentTarget;
+        if (pairCheckTempArray.length < 2) {
+            element.classList.add('card--front');
+            element.classList.remove('card--back');
+            pairCheckTempArray.push(element);
+            if (pairCheckTempArray.length === 2) {
+                if (!checkIsPair()) {
+                    console.log('nem pár');
+                    setTimeout(() => {
+                        pairCheckTempArray.forEach(item => {
+                            item.classList.remove('card--front');
+                            item.classList.add('card--back');
+                        })
+                        pairCheckTempArray = [];
+                    }
+                        , 2500);
+                } else {
+                    console.log('pár');
+                    pairCheckTempArray.forEach(item => {
+                        item.removeEventListener('click', clickEvent, true);
+                    });
+                    pairCheckTempArray = [];
+                    if (Array.from(cards).every(item => [...item.classList].includes('card--front'))) {
+                        //timer stop
+                        console.log('vege');
+                    }
+                }
+            }
+        }
+    }
+    (() => {
+        cards.forEach(element => {
+            addCardItem(element);
+            addEventToCard(element);
+        })
+    }
+    )();
 
-const addAllEvent = () => {
-    cards.forEach(element =>
-        element.addEventListener('click', () => element.classList.add('card--front'))
-    );
+
+
 }
-
-const iconArray = ['save', 'caret-right', 'envelope', 'paper'];
-
-(() => addAllEvent())();
