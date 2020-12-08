22059574a3e1926afab5488e5e0ab5245fffe0c3
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-12-08 14:11:09 +0100

    add css and card images

--- js/pair-game.js
+++ js/pair-game.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+const cards = document.querySelectorAll('.card');
+
+const addAllEvent = () => {
+    cards.forEach(element =>
+        element.addEventListener('click', () => element.classList.add('card--front'))
+    );
+}
+
+(() => addAllEvent())();
